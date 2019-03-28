42547ac952fc007ca2140b7aeb9f725cf77f214a
Ryan Holeman <ryan@hackgnar.com>
2019-03-29 08:20:10 +1100

    updated test script a bit

--- test.sh
+++ test.sh
@@ -2,4 +2,4 @@
 sudo apt-get install -y python-dnspython
 curl -L -O https://opendata.rapid7.com/sonar.udp/2019-02-04-1549285686-udp_dns_53.csv.gz
 gunzip 2019-02-04-1549285686-udp_dns_53.csv.gz
-for i in $(awk -F',' '{print $2}' 2019-02-04-1549285686-udp_dns_53.csv); do printf "%q\n" "echo -n $i | python dns_crawl.py trello.com >> results.csv"; done | xargs -I CMD --max-procs=500 bash -c CMD
+for i in $(awk -F',' '{print $2}' 2019-02-04-1549285686-udp_dns_53.csv); do printf "%q\n" "echo -n $i | python dns_crawl.py $1 >> results.csv"; done | xargs -I CMD --max-procs=1000 bash -c CMD
